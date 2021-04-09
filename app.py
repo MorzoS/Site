38afb45051f9053d0945e15d3e66e1759d2d8fa6
MorzoS <svenvanluijk123@gmail.com>
2021-04-09 21:38:16 +0200

    init

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ db = SQLAlchemy(app)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
