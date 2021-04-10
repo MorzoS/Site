729d7ecceb74649cdd1609ea2844c83c72d1da7d
MorzoS <svenvanluijk123@gmail.com>
2021-04-10 03:28:38 +0200

    init

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
+SHARED_TEMPLATE_FOLDER = "wikiflex/beeld/sh_templates"
+SHARED_STATIC_FOLDER = "wikiflex/beeld/sh_static"
 
-
+app = Flask(__name__, template_folder=SHARED_TEMPLATE_FOLDER, static_folder=SHARED_STATIC_FOLDER)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('base.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
