3a05f0c0fc97b2c44d587ddd807e96cc66d1781e
Micha Kuipers <michakuipers@hotmail.nl>
2021-04-10 20:33:48 +0200

    core init (work in progress)

---
+++ wikiflex/core/__init__.py
@@ -0,0 +1,34 @@
+from flask import url_for
+
+
+
+class NavBar:
+    _default_bar = None
+
+    class NavElement
+
+        def __init__(self, text: str, link: str = "#", active: bool = False):
+            self.text = text
+            self.link = link
+            self.active = active
+        
+        def __str__(self):
+            return f"[{self.text}, {self.link}, {self.active}]"
+
+    def __init__(self, *elements):
+        self.elements = elements
+
+    def __str__(self):
+        return str(self.elements)
+
+    @classmethod
+    def _default_bar(cls, active_page=None):
+        if not cls._default_bar:
+            cls._default_bar = NavBar(
+                cls.NavElement("home", url_for("home.home"))
+                cls.NavElement("404", "Anywhere")
+            )
+        for element in cls._default_bar.elements:
+            element.active = True if element.text == active_page else False
+
+        return cls._default_bar
