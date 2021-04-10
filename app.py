b52796291003db927cabf2a42384ae73d71b16bb
MorzoS <svenvanluijk123@gmail.com>
2021-04-10 04:20:39 +0200

    linking

--- app.py
+++ app.py
@@ -4,11 +4,21 @@ from flask_sqlalchemy import SQLAlchemy
 SHARED_TEMPLATE_FOLDER = "wikiflex/beeld/sh_templates"
 SHARED_STATIC_FOLDER = "wikiflex/beeld/sh_static"
 
+def page_not_found(e):
+	return render_template("404.html"), 404
+
+
+def general_error(e):
+	return render_template("bad_request.html")
+
 app = Flask(__name__, template_folder=SHARED_TEMPLATE_FOLDER, static_folder=SHARED_STATIC_FOLDER)
 
 @app.route('/')
-def home():
-    return render_template('base.html')
+def basic():
+    return render_template('basic.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
+import wikiflex
+from wikiflex.beeld import home
+app.register_blueprint(home.blueprint)
