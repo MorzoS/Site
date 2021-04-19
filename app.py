481cadf57f953646be765017582cfd1e6e1ba575
MorzoSL <Svenvanluijk123@gmail.com>
2021-04-19 23:59:23 +0200

    app changes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import wikiflix
 SHARED_TEMPLATE_FOLDER = "wikiflex/beeld/sh_templates"
 SHARED_STATIC_FOLDER = "wikiflex/beeld/sh_static"
 
-def page_not_found(e):
+def pagina_niet_gevonden(e):
 	return render_template("404.html"), 404
 
 
@@ -20,14 +20,17 @@ def main():
 
 def app_aanmaken():
     app = Flask(__name__, template_folder=SHARED_TEMPLATE_FOLDER, static_folder=SHARED_STATIC_FOLDER)
-    app.register_error_handler(404, page_not_found)
+    app.register_error_handler(404, pagina_niet_gevonden)
     app.register_error_handler(HTTPException, general_error)
 
     #blueprints laden
 
     app.register_blueprint(blueprint, url_prefix="")
 
+    app.run(debug=True)
+
     return app
 
     if __name__ == "__main__":
-        main()
+        app_aanmaken()
+        app.run(debug=True)
